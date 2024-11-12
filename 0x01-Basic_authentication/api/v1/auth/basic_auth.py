ee88a5b77fe0ae1e3c5d8c440b3087bdd8662c36
ZiadAS40 <zead4036@gmail.com>
2024-11-12 21:53:48 +0200

    fix some buggs

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """ making the class for basic authentication
 """
+from .auth import Auth
 
 
-class BasicAuth:
+class BasicAuth(Auth):
     """
     a class for the basic authentication
     """
