26bd190344dc710754c96d81a4a747dd2003d75e
ZiadAS40 <zead4036@gmail.com>
2024-11-12 22:50:08 +0200

    add task number 10

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,8 +2,10 @@
 """ making the class for basic authentication
 """
 from .auth import Auth
+from models.user import User
 import base64
 import binascii
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -58,3 +60,24 @@ class BasicAuth(Auth):
         username = decoded_base64_authorization_header.split(":")[0]
         password = decoded_base64_authorization_header.split(":")[1]
         return username, password
+
+    def user_object_from_credentials(self,
+                                     user_email:
+                                     str, user_pwd:
+                                     str) -> TypeVar('User'):
+        """make a user instance with its credentials
+        """
+        if user_email is None or user_pwd is None:
+            return None
+
+        if type(user_pwd) is not str or type(user_email) is not str:
+            return None
+
+        users = User.search({'email': user_email})
+
+        if not users:
+            return None
+
+        if users[0].is_valid_password(user_pwd):
+            return users[0]
+        return None
