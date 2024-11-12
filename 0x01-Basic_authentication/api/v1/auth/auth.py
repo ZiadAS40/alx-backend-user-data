e07c575b501d5616c044f0cf398aeb7875aab610
ZiadAS40 <zead4036@gmail.com>
2024-11-12 14:06:55 +0200

    add the task number 3

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,29 @@
+#!usr/bin/env python3
+""" making the class for the authentication
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """
+    the class used for the authentication
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        just return Fals for now
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        just return None for now
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """"
+        just return None for now
+        """
+        return None
