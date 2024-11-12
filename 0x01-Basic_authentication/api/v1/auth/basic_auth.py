d8602930a72d704e9c9500f5c5763d46089eaae4
ZiadAS40 <zead4036@gmail.com>
2024-11-12 22:31:33 +0200

    add task number 9

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -41,3 +41,20 @@ class BasicAuth(Auth):
             return res.decode('utf-8')
         except (binascii.Error, UnicodeDecodeError):
             return None
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header:
+                                 str) -> (str, str):
+        """ extract the user credentials
+        """
+        if decoded_base64_authorization_header is None:
+            return None, None
+
+        if type(decoded_base64_authorization_header) is not str:
+            return None, None
+
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+        username = decoded_base64_authorization_header.split(":")[0]
+        password = decoded_base64_authorization_header.split(":")[1]
+        return username, password
