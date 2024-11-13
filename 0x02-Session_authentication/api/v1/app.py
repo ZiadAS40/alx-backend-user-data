861dffa7620ffe16970ae56fc1686cabc242911e
ZiadAS40 <zead4036@gmail.com>
2024-11-13 21:54:37 +0200

    second try of fixing the bug in task number 2

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -19,7 +19,7 @@ if os.getenv("AUTH_TYPE") == 'auth':
 elif os.getenv("AUTH_TYPE") == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
-elif os.getenv("AUTH_TYPE") == 'session_auth0':
+elif os.getenv("AUTH_TYPE") == 'session_auth':
     from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
 
