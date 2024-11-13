f80d6a45ea6cbfa9cb66562348dc1a780a6497d5
ZiadAS40 <zead4036@gmail.com>
2024-11-13 22:02:04 +0200

    adding the task number 3

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,6 +2,7 @@
 """ making the class for session authentication
 """
 from .auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
@@ -10,4 +11,17 @@ class SessionAuth(Auth):
     for the authenticatio
     which is 'Session authenticatio
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        creating a sesseion and store it
+        """
+        if not user_id or type(user_id) is not str:
+            return None
+
+        session_id = str(uuid.uuid4())
+
+        self.user_id_by_session_id[session_id] = user_id
+
+        return session_id
