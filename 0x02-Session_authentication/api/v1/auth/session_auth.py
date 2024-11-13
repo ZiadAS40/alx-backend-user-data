5644dfd921d69026826b50838de431fa8a059cdb
ZiadAS40 <zead4036@gmail.com>
2024-11-13 21:44:13 +0200

    adding the task number 2

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" making the class for session authentication
+"""
+from .auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    a Class for the new mechanism
+    for the authenticatio
+    which is 'Session authenticatio
+    """
+    pass
