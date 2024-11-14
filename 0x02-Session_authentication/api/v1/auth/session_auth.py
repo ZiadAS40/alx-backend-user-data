b2afa4f704479a4faa158000d35c3113cb9c6a66
ZiadAS40 <zead4036@gmail.com>
2024-11-14 10:22:51 +0200

    adding task number four

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -25,3 +25,13 @@ class SessionAuth(Auth):
         self.user_id_by_session_id[session_id] = user_id
 
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        return user id based on
+        a session
+        """
+        if session_id is None or type(session_id) != str:
+            return None
+
+        return self.user_id_by_session_id.get(session_id)
