eeb1e75cd98625124806c338f610a13ffa8f93ea
ZiadAS40 <zead4036@gmail.com>
2024-11-14 12:29:39 +0200

    fix the task number 6s bugg

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -63,14 +63,12 @@ def handle_before_req():
     else:
         return
 
-    if auth.current_user(request) is None:
-        abort(403)
-
-    if auth.authorization_header(request) is None:
+    session_co = auth.session_cookie(request)
+    if not auth.authorization_header(request) and not session_co:
         abort(401)
 
-    if auth.authorization_header(request) and auth.session_cookie(request):
-        return None, abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
 
     request.current_user = auth.current_user(request)
 
